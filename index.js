f803fe76ffc13ebdea94667d99c077df7f8f1740
efar716 <110000165+efar716@users.noreply.github.com>
2023-04-16 23:00:14 +1200

    New theme

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
 
 function goHome(){
 
-
-    const x =  document.getElementById("home");
-    const y =  document.getElementById("background");
-    y.classList.add('visuallyhidden');
-    setTimeout(() => {  y.classList.add('hidden') }, 1000);
+  transition("home","background");
     
-    setTimeout(function () {
-        x.classList.remove('hidden');
-      }, 1000);
-    setTimeout(function () {
-          x.classList.remove('visuallyhidden');
-        }, 1000 +50);
-      
-
 
 
 }
  
 
 
+function transition(showedID,hiddenID){
+  const x =  document.getElementById(showedID);
+  const y =  document.getElementById(hiddenID);
+  y.classList.add('visuallyhidden');
+  setTimeout(() => {  y.classList.add('hidden') }, 1000);
+  
+  setTimeout(function () {
+      x.classList.remove('hidden');
+    }, 1000);
+  setTimeout(function () {
+        x.classList.remove('visuallyhidden');
+      }, 1000 +50);
 
+}
 
 
 
 
 
 function goBackground(){
-    const x =  document.getElementById("home");
-    const y =  document.getElementById("background");
-    x.classList.add('visuallyhidden');
-    setTimeout(() => {  x.classList.add('hidden') }, 1000);
-
     
-        y.classList.remove('hidden');
-        setTimeout(function () {
-          y.classList.remove('visuallyhidden');
-        }, 1000);
-      
-
+    transition("background","home");
+    
 }
