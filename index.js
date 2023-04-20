d3f1f491e884278d448cc144581ebf9d9d3b5d71
efar716 <110000165+efar716@users.noreply.github.com>
2023-04-20 17:32:30 +1200

    contact info

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 
+
+const home = document.getElementById("home");
+const book = document.getElementById("books");
+const background = document.getElementById("background");
+const contact = document.getElementById("contact");
+
+const elementList = [home,book, background,contact];
 function goHome(){
 
-  transition("home","background");
+  transition("home");
     
 
 
@@ -9,9 +16,19 @@ function goHome(){
  
 
 
-function transition(showedID,hiddenID){
-  const x =  document.getElementById(showedID);
-  const y =  document.getElementById(hiddenID);
+function transition(showedID){
+  let x =  document.getElementById(showedID);
+  let y;
+  for(let i = 0; i < elementList.length;i++ ){
+
+    if (!elementList[i].classList.contains("visuallyhidden")) { y = elementList[i];}
+
+
+
+  }
+
+  
+  
   y.classList.add('visuallyhidden');
   setTimeout(() => {  y.classList.add('hidden') }, 1000);
   
@@ -22,14 +39,23 @@ function transition(showedID,hiddenID){
         x.classList.remove('visuallyhidden');
       }, 1000 +50);
 
-}
-
-
-
-
+} 
 
 function goBackground(){
-    
-    transition("background","home");
-    
+    transition("background");
+}
+
+function goBooks() {
+transition("books");
+}
+
+function goContacts() {
+  transition("contact");
+  }
+
+
+function goMovies(){
+  transition("movies");
+
+
 }
